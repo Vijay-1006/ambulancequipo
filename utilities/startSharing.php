f592d4b92a679cc9ffe7fa5c5c4a93144656d3e2
Krishna Paanchajanya <60803066+PAANCHAJANYA@users.noreply.github.com>
2021-12-18 11:17:18 +0530

    Update startSharing.php

--- utilities/startSharing.php
+++ utilities/startSharing.php
@@ -1,23 +1,30 @@
 <?php
 require_once("config.php");
+echo "connected";
 $query1 = $con->prepare("SELECT * FROM location WHERE liveStatus='active' AND driverID=:driverID");
 $query1->bindParam(":driverID", $_POST["driverID"]);
 $query1->execute();
+echo "checked rows";
 if($query1->rowCount()==0)
 {
+    echo "entered if";
     $query2 = $con->prepare("INSERT INTO location (driverID, latitude, longitude, patientID, liveStatus, timeBegin) VALUES (:driverID, :latitude, :longitude, :patientID, 'active', CURRENT_TIMESTAMP)");
     $query2->bindParam(":driverID", $_POST["driverID"]);
     $query2->bindParam(":latitude", $_POST["latitude"]);
     $query2->bindParam(":longitude", $_POST["longitude"]);
     $query2->bindParam(":patientID", $_POST["patientID"]);
     $query2->execute();
+    echo "over if";
 }
 else
 {
+    echo "entered else";
     $query3 = $con->prepare("UPDATE location SET latitude=:latitude, longitude=:longitude WHERE liveStatus='active' AND driverID=:driverID");
     $query3->bindParam(":driverID", $_POST["driverID"]);
     $query3->bindParam(":latitude", $_POST["latitude"]);
     $query3->bindParam(":longitude", $_POST["longitude"]);
     $query3->execute();
+    echo "over else";
 }
+echo "over";
 ?>
