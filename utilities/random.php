a5b7b1cb04bc90e224d1d110eaebac9bc2a5f003
Krishna Paanchajanya <60803066+PAANCHAJANYA@users.noreply.github.com>
2021-12-18 11:24:35 +0530

    Update random.php

--- utilities/random.php
+++ utilities/random.php
@@ -5,12 +5,5 @@ $query1 = $con->prepare("SELECT * FROM location WHERE liveStatus='active' AND dr
 $query1->bindParam(":driverID", "driverID");
 $query1->execute();
 echo "checked rows";
-echo $query1->rowCount();
-if($query1->rowCount()==0)
-{
-    echo "entered if";
-    
-    echo "over if";
-}
 echo "over";
 ?>
