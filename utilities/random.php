7af95f3ba8dd0ebea27f7904645503a026da309a
Krishna Paanchajanya <60803066+PAANCHAJANYA@users.noreply.github.com>
2021-12-18 10:48:56 +0530

    Update random.php

--- utilities/random.php
+++ utilities/random.php
@@ -1,3 +1,16 @@
 <?php
 echo "Hi";
+ob_start();
+date_default_timezone_set("Asia/Kolkata");
+try
+{
+    /*Developer:$con=new PDO("mysql:dbname=ambulancequipo;host=localhost","root","");*/
+    $con = new PDO("sqlsrv:server = tcp:ambulancequipo-server.database.windows.net,1433; Database = ambulancequipo", "ambulancequipo-server-admin", "Kuppa#304");
+    $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connected";
+}
+catch(PDOException $e)
+{
+  echo $e;
+}
 ?>
