c454003cde97421fbda13d5a5eec4b922e58cdfc
Krishna Paanchajanya <60803066+PAANCHAJANYA@users.noreply.github.com>
2021-12-18 10:46:53 +0530

    Update random.php

--- utilities/random.php
+++ utilities/random.php
@@ -1,6 +1,3 @@
 <?php
-require_once("config.php");
-$query = $con->prepare("SELECT * FROM location WHERE driverID='D001' and liveStatus='active'");
-$query->execute();
-echo $query->rowCount();
+echo "Hi";
 ?>
