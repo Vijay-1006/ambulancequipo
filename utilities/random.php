7f2a44f8ed319d0bff89299b77dab5172fd1f28c
Krishna Paanchajanya <60803066+PAANCHAJANYA@users.noreply.github.com>
2021-12-18 11:22:40 +0530

    Update random.php

--- utilities/random.php
+++ utilities/random.php
@@ -5,15 +5,11 @@ $query1 = $con->prepare("SELECT * FROM location WHERE liveStatus='active' AND dr
 $query1->bindParam(":driverID", "driverID");
 $query1->execute();
 echo "checked rows";
+echo $query1->rowCount();
 if($query1->rowCount()==0)
 {
     echo "entered if";
-    $query2 = $con->prepare("INSERT INTO location (driverID, latitude, longitude, patientID, liveStatus, timeBegin) VALUES (:driverID, :latitude, :longitude, :patientID, 'active', CURRENT_TIMESTAMP)");
-    $query2->bindParam(":driverID", "driverID");
-    $query2->bindParam(":latitude", "latitude");
-    $query2->bindParam(":longitude", "longitude");
-    $query2->bindParam(":patientID", 7);
-    $query2->execute();
+    
     echo "over if";
 }
 echo "over";
