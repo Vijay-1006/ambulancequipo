8a768e9362f81cc49e6ef2eb2a59b4ff79093de0
Krishna Paanchajanya <60803066+PAANCHAJANYA@users.noreply.github.com>
2021-12-18 10:42:09 +0530

    Update startSharing.php

--- utilities/startSharing.php
+++ utilities/startSharing.php
@@ -1,16 +1,5 @@
 <?php
-ob_start();
-date_default_timezone_set("Asia/Kolkata");
-try
-{
-    /*Developer:$con=new PDO("mysql:dbname=ambulancequipo;host=localhost","root","");*/
-    $con = new PDO("sqlsrv:server = tcp:ambulancequipo-server.database.windows.net,1433; Database = ambulancequipo", "ambulancequipo-server-admin", "Kuppa#304");
-    $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch(PDOException $e)
-{
-    echo $e;
-}
+require_once("config.php");
 $query1 = $con->prepare("SELECT * FROM location WHERE liveStatus='active' AND driverID=:driverID");
 $query1->bindParam(":driverID", $_POST["driverID"]);
 $query1->execute();
