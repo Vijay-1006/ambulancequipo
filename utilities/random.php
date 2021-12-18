a5a4537c32c2215473f6f6d6e3940a300ca5c836
Krishna Paanchajanya <60803066+PAANCHAJANYA@users.noreply.github.com>
2021-12-18 10:59:45 +0530

    Update random.php

--- utilities/random.php
+++ utilities/random.php
@@ -7,6 +7,7 @@ try
     /*Developer:$con=new PDO("mysql:dbname=ambulancequipo;host=localhost","root","");*/
     $con = new PDO("sqlsrv:server = tcp:ambulancequipo-server.database.windows.net,1433; Database = ambulancequipo", "ambulancequipo-server-admin", "Kuppa#304");
     $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "connected";
 }
 catch(PDOException $e)
 {
@@ -14,4 +15,11 @@ catch(PDOException $e)
 }
 $query = $con->prepare("SELECT * FROM location WHERE liveStatus='active' AND driverID='D001'");
 $query->execute();
+if($query->rowCount()==0)
+{
+    $query1 = $con->prepare("INSERT INTO location (driverID, latitude, longitude, patientID, liveStatus, timeBegin) VALUES ('D001', '1.1', '1.2', 1, 'active', CURRENT_TIMESTAMP)");
+    $query1->execute();
+    echo "executed";
+}
+echo "Bye";
 ?>
