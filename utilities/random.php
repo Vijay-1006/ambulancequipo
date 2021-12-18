94c40ffae68d924221f6f6b06397a0aba4a2bdcf
Krishna Paanchajanya <60803066+PAANCHAJANYA@users.noreply.github.com>
2021-12-18 11:42:19 +0530

    Update random.php

--- utilities/random.php
+++ utilities/random.php
@@ -5,6 +5,8 @@ $driverID = "driverID";
 $query1 = $con->prepare("SELECT * FROM location WHERE liveStatus='active' AND driverID=:driverID");
 $query1->bindParam(":driverID", $driverID);
 $query1->execute();
+$sqldata1 = $query1->fetchAll(PDO::FETCH_ASSOC);
+echo (string)$sqldata1;
 echo "checked rows\n";
 echo $query1->rowCount();
 echo "over\n";
