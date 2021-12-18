9b96a657b00ba499bc192225cfefd7ce1d183489
Krishna Paanchajanya <60803066+PAANCHAJANYA@users.noreply.github.com>
2021-12-18 11:26:15 +0530

    Update random.php

--- utilities/random.php
+++ utilities/random.php
@@ -1,9 +1,7 @@
 <?php
 require_once("config.php");
 echo "connected";
-$query1 = $con->prepare("SELECT * FROM location WHERE liveStatus='active' AND driverID=:driverID");
-$query1->bindParam(":driverID", "driverID");
-$query1->execute();
+
 echo "checked rows";
 echo "over";
 ?>
